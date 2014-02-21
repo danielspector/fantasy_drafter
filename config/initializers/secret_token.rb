42fe2ad1c9a8e8d02855fb317dc78bc80014ca5d
danielspector <danielyspector@gmail.com>
2014-02-21 11:37:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FantasyDrafter::Application.config.secret_key_base = '4e78bfa3d2a7393d92d375f7cf30423a6a19600a4b0b759170fa006b6c6a32347a543164ce83feaf0c34270e650998cbfbfabfd49acb5515e8046d4f97109989'
